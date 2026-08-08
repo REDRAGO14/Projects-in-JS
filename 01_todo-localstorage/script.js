b407f6a67558ee510d83ef49cf8d496b0f1f6c39
REDRAGON <148379059+REDRAGO14@users.noreply.github.com>
2026-08-08 01:00:24 -0700

    TodoList: save tasks to local storage and load tasks from local storage to and arrays as soon as DOMContentLoaded eventlistener trigered

--- 01_todo-localstorage/script.js
+++ 01_todo-localstorage/script.js
@@ -1,8 +1,11 @@
+document.addEventListener("DOMContentLoaded", function(){
 let button = document.getElementById("add-task-btn")
 let input = document.getElementById("todo-input")
 let todos = document.getElementById("todo-list")
 
-let tasks =[]
+let tasks = JSON.parse(localStorage.getItem("tasks")) ||[]
+tasks.forEach(task => renderTask(task))
+
 
 button.addEventListener("click", function(){
     let inputText = input.value.trim()
@@ -15,7 +18,20 @@ button.addEventListener("click", function(){
     }
 
     tasks.push(newTask)
+    saveToLocalstorage()
+    console.log(tasks);
+    
     input.value = ""
     
+
     
+})
+function renderTask(task){
+    let todo =document.createElement('li').textContent = task.task
+    todos.append(todo)
+}
+
+function saveToLocalstorage(){
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+}
 })
