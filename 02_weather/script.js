961452057f18bcf3dc17b1990ff44d70112d2d17
REDRAGON <148379059+REDRAGO14@users.noreply.github.com>
2026-08-09 13:40:43 -0700

    Weather App: learned the 2 golden rule when working with api. 1) server is in another contiunent 2) server will throw an error we sould be aware of this so 1st we need to handle a potential error and 2nd we need to use asynchronous funciton

--- 02_weather/script.js
+++ 02_weather/script.js
@@ -8,18 +8,32 @@ document.addEventListener("DOMContentLoaded",()=>{
     let errorDisplay = document.getElementById("error-message")
 
     
-    getWeatherBtn.addEventListener("click", function (){
+    getWeatherBtn.addEventListener("click", async function (){
         let city = cityInput.value.trim()
         if(!city) return;
         
-        
+        //server is in other continent
+        //server will throw an error
+
+        try {
+            const weatherData = await fetchWeatherData(city)
+            getWeatherData(weatherData)
+        } catch (error) {
+            displayError()
+        }
     })
 
-     
+    function fetchWeatherData(city){
+
+    }
+
+
+    function getWeatherData(weatherData){
+
+    }
 
     function displayError(){
         weatherInfo.classList.add("hidden")
         errorDisplay.classList.remove("hidden")
     }
-    
 })
