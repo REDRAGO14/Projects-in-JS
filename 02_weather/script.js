a5de0abaf1486d987b514bbf79ac7dd9ba073129
REDRAGON <148379059+REDRAGO14@users.noreply.github.com>
2026-08-09 13:28:52 -0700

    Weather App: grabed necerrey element from the doucment and stored them in to variable

---
+++ 02_weather/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    let cityInput = document.getElementById("city-input")
+    let getWeatherBtn = document.getElementById("get-weather-btn")
+    let weatherInfo = document.getElementById("weather-info")
+    let cityNameDisplay = document.getElementById("city-name")
+    let temperatureDisplay = document.getElementById("temperature")
+    let descriptionDisplay = document.getElementById("description")
+    let errorDisplay = document.getElementById("error-message")
+
+    
+})
