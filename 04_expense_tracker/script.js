441f9ed9111a727b1fc58c6251e3c4769a33fc4a
REDRAGON <148379059+REDRAGO14@users.noreply.github.com>
2026-08-10 07:36:23 -0700

    ExpenseTracker: grabed elements that are needed after the DOMContentLoaded

---
+++ 04_expense_tracker/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    let expenseNameInput = document.getElementById("expense-name")
+    let expenseAmountInput = document.getElementById("expense-amount")
+    let expenseListDisplay = document.getElementById("expense-list")
+    let expenseTotalDisplay = document.getElementById("total-amount")
+})
