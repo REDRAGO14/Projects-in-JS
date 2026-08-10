7be13905e47c80de356b69be2f589ef0cee4fd75
REDRAGON <148379059+REDRAGO14@users.noreply.github.com>
2026-08-10 07:48:54 -0700

    ExpenseTracker: implement an eventlistener to the form submit to grab the input values and push them to expenses array after constracting an object with the input

--- 04_expense_tracker/script.js
+++ 04_expense_tracker/script.js
@@ -1,6 +1,26 @@
 document.addEventListener("DOMContentLoaded", ()=>{
+    let expenseForm = document.getElementById("expense-form")
     let expenseNameInput = document.getElementById("expense-name")
     let expenseAmountInput = document.getElementById("expense-amount")
     let expenseListDisplay = document.getElementById("expense-list")
     let expenseTotalDisplay = document.getElementById("total-amount")
+
+    let expenses = []
+
+    expenseForm.addEventListener("submit", (e) =>{
+        e.preventDefault()
+        let ExpenseName = expenseNameInput.value.trim()
+        let ExpenseAmount = parseFloat(expenseAmountInput.value.trim())
+
+        if(!ExpenseName == "" && !isNaN(ExpenseAmount) && ExpenseAmount > 0){
+            const newExpense = {
+                id: Date.now(),
+                name: ExpenseName,
+                amount: ExpenseAmount
+            }
+
+            expenses.push(newExpense)
+            saveExpenseToLocal()
+        }
+    })
 })
