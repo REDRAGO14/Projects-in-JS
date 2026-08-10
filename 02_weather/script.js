95e8ae911cc93529dd4412a81887096db1ef4aca
REDRAGON <148379059+REDRAGO14@users.noreply.github.com>
2026-08-10 00:24:45 -0700

    grabed all needed element once the dom content is loaded

--- 02_weather/script.js
+++ 02_weather/script.js
@@ -1,54 +1,8 @@
-document.addEventListener("DOMContentLoaded",()=>{
+document.addEventListener("DOMContentLoaded", ()=>{
+    let getWeatherBTN = document.getElementById("get-weather-btn")
     let cityInput = document.getElementById("city-input")
-    let getWeatherBtn = document.getElementById("get-weather-btn")
-    let weatherInfo = document.getElementById("weather-info")
+    let WeatherInfo = document.getElementById("weather-info")
     let cityNameDisplay = document.getElementById("city-name")
-    let temperatureDisplay = document.getElementById("temperature")
-    let descriptionDisplay = document.getElementById("description")
-    let errorDisplay = document.getElementById("error-message")
-
-    let API_KEY = "c5dc9142934fa9a0f84a112b3d1d4f25"
-    getWeatherBtn.addEventListener("click", async function (){
-        let city = cityInput.value.trim()
-        if(!city) return;
-        
-        //server is in other continent
-        //server will throw an error
-
-        try {
-            const weatherData = await fetchWeatherData(city)
-            getWeatherData(weatherData)
-        } catch (error) {
-            displayError()
-        }
-    })
-
-    async function fetchWeatherData(city){
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
-
-        let response =  await fetch(url)
-
-        if(!response.ok){
-            throw new Error("request failed")
-        }
-    
-        return response.json()
-    }
-
-
-    function getWeatherData(data){
-        console.log(data);
-        errorDisplay.classList.add("hidden")
-        weatherInfo.classList.remove("hidden")
-        cityNameDisplay.textContent = `CITY: ${data.name}`;
-        temperatureDisplay.textContent = `Temprature: ${data.main.temp}`
-        descriptionDisplay.textContent = `Description: ${data.weather[0].description}`
-        
-        
-    }
-
-    function displayError(){
-        weatherInfo.classList.add("hidden")
-        errorDisplay.classList.remove("hidden")
-    }
+    let cityTempDisplay= document.getElementById("temperature")
+    let weatherDescDisplay= document.getElementById("discription")
 })
