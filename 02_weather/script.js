c5906045e8f140fb51aa6fe3542e82f35c9b3b63
REDRAGON <148379059+REDRAGO14@users.noreply.github.com>
2026-08-10 00:03:42 -0700

    Weather App: finished implementing the fetchWeatherData(city) function using the fetch(url) method that retrun the Response object and the i returned the response ans a json also if the process fail the function will throw an error

--- 02_weather/script.js
+++ 02_weather/script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded",()=>{
     let descriptionDisplay = document.getElementById("description")
     let errorDisplay = document.getElementById("error-message")
 
-    
+    let API_KEY = "c5dc9142934fa9a0f84a112b3d1d4f25"
     getWeatherBtn.addEventListener("click", async function (){
         let city = cityInput.value.trim()
         if(!city) return;
@@ -23,8 +23,16 @@ document.addEventListener("DOMContentLoaded",()=>{
         }
     })
 
-    function fetchWeatherData(city){
+    async function fetchWeatherData(city){
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
 
+        let response =  await fetch(url)
+
+        if(!response.ok){
+            throw new Error("request failed")
+        }
+    
+        return response.json()
     }
 
 
